83c4e64e24811d592a12e1db12893798d0024308
Aleksandar Atanackovic <atanackovic096@gmail.com>
2025-11-13 10:30:58 +0100

    Playing with different Go packages

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
-	fmt.Println("Test message!")
+	fmt.Println("Time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println("I like pie", math.Pi)
 }
